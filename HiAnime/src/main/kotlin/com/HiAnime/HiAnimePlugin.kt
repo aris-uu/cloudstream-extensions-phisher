c5c872d4a37b2badb64e3d55e206fad9990cfe51
arisu <136294347+aris-uu@users.noreply.github.com>
2025-12-26 17:35:19 +0700

    Add HIANIMEZ_TO server to ServerList enum

--- HiAnime/src/main/kotlin/com/HiAnime/HiAnimePlugin.kt
+++ HiAnime/src/main/kotlin/com/HiAnime/HiAnimePlugin.kt
@@ -10,6 +10,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 enum class ServerList(val link: Pair<String, Boolean>) {
     HIANIMEZ_IS("https://hianimez.is" to true),
     BEST("https://hianime.to" to true),
+    HIANIMEZ_TO("https://hianimez.to" to true),
     HIANIME_NZ("https://hianime.nz" to true),
     HIANIME_BZ("https://hianime.bz" to true),
     HIANIME_PE("https://hianime.pe" to true),
